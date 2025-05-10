118aa56321ab075eedc22662f994b50988c147a0
molor824 <molormargaderdene@gmail.com>
2025-05-10 19:25:46 +0800

    Implement admin template for tuuguu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router";
 import NotFound from "./routes/not-found";
+import { useUser } from "./providers/user-provider";
+import { Spin } from "antd";
 
 const Home = lazy(() => import("./routes/home"));
 const About = lazy(() => import("./routes/about"));
@@ -9,16 +11,29 @@ const DefaultLayout = lazy(() => import("./routes/layout"));
 const Login = lazy(() => import("./routes/login"));
 const Register = lazy(() => import("./routes/register"));
 
+const ExampleAdmin = <h1>Admin</h1>;
+
 function App() {
+  const { user, loading } = useUser();
+
+  if (!loading) {
+    return <Spin size="large" />;
+  }
   return (
     <Routes>
       <Route element={<DefaultLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        {!user?.isAdmin ? (
+          <>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        ) : (
+          <Route index element={ExampleAdmin} />
+        )}
       </Route>
     </Routes>
   );
